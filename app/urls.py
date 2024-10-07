98a0847d4c73dda2bec0549882c1d8df4cefd881
Raphael Okere <raphaelokere@Raphaels-MacBook-Pro.local>
2024-10-06 21:04:10 -0400

    updating urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),  # Django admin route
+    path('auth/', include('social_django.urls', namespace='social')),  # Social authentication URLs
 ]
