6cdd5603af32ca254add2eb289faac7320ac7796
Raphael Okere <raphaelokere@Raphaels-MacBook-Pro.local>
2024-10-06 21:17:49 -0400

    updating configs

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin  # This imports the Django admin module
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Django admin route
+    path('admin/', admin.site.urls),  # Admin URL
     path('auth/', include('social_django.urls', namespace='social')),  # Social authentication URLs
 ]
